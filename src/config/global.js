59596767efa10e0a2921e45b4d1d25599b082625
manuelechavarria <maechavarriaor@gmail.com>
2025-09-16 19:27:45 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -258,7 +258,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/Fisica_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
